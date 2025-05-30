c768cde3598c2b3d75817bd549fe0ae9318e00ac
Documenter.jl <documenter@juliadocs.github.io>
2025-05-30 15:05:50 +0000

    build based on ac933bd

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#BergerOligerAMR","page":"Home","title":"BergerOligerAMR","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for BergerOligerAMR.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
