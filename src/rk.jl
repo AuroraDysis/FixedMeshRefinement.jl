e6887f0404163d18f076f7ef90c385f8aa933107
Zhen Zhong <auroradysis@gmail.com>
2025-06-07 00:54:13 +0100

    Update rk.jl

--- src/rk.jl
+++ src/rk.jl
@@ -31,8 +31,11 @@ function rk4_mongwane!(
     f::Function, level::Level{NumState,NumDiagnostic}
 ) where {NumState,NumDiagnostic}
     (; u, u_p, rhs, tmp, k, t, dt) = level
+    sixth_dt = dt / 6
+    third_dt = dt / 3
+    half_dt = dt / 2
 
-    k1, k2, k3, k4 = level.k
+    k1, k2, k3, k4 = k
 
     isrt = level.is_base_level ? 1 : 1 + level.num_buffer_points
     iend = if level.is_base_level
@@ -44,34 +47,26 @@ function rk4_mongwane!(
     @. u_p = u
     level.t = t
     f(level, rhs, u)
-    for v in 1:NumState
-        k1[v][isrt:iend] = rhs[v][isrt:iend] * dt
-    end
-    @. u += k1 * (1 / 6)
+    k1[isrt:iend, :] .= rhs[isrt:iend, :]
+    @. u += k1 * sixth_dt
 
-    @. tmp = u_p + k1 * (1 / 2)
-    level.t = t + 0.5 * dt
+    @. tmp = u_p + half_dt * k1
+    level.t = t + half_dt
     f(level, rhs, tmp)
-    for v in 1:NumState
-        k2[v][isrt:iend] = rhs[v][isrt:iend] * dt
-    end
-    @. u += k2 * (1 / 3)
+    k2[isrt:iend, :] .= rhs[isrt:iend, :]
+    @. u += third_dt * k2
 
-    @. tmp = u_p + k2 * (1 / 2)
-    level.t = t + 0.5 * dt
+    @. tmp = u_p + half_dt * k2
+    level.t = t + half_dt
     f(level, rhs, tmp)
-    for v in 1:NumState
-        k3[v][isrt:iend] = rhs[v][isrt:iend] * dt
-    end
-    @. u += k3 * (1 / 3)
+    k3[isrt:iend, :] .= rhs[isrt:iend, :]
+    @. u += third_dt * k3
 
     @. tmp = u_p + k3
     level.t = t + dt
     f(level, rhs, tmp)
-    for v in 1:NumState
-        k4[v][isrt:iend] = rhs[v][isrt:iend] * dt
-    end
-    @. u += k4 * (1 / 6)
+    k4[isrt:iend, :] .= rhs[isrt:iend, :]
+    @. u += sixth_dt * k4
     return level.t = t + dt
 end
 
