3d6821720e28961f814b339af512818fd34b4eb3
Zhen Zhong <auroradysis@gmail.com>
2025-06-06 20:00:27 +0100

    Update Boundary.jl

--- src/Boundary.jl
+++ src/Boundary.jl
@@ -1,16 +1,18 @@
 module Boundary
 
-function ApplyPeriodicBoundaryCondition!(grid)
-    lev1fs = grid.levels[1]
-    num_total_points = grid.levels[1].num_total_points
-    num_buffer_points = grid.levels[1].num_buffer_points
-    for v = 1:grid.NumState
-        u = lev1fs.u[v]
-        for i = 1:num_buffer_points
-            u[i] = u[num_total_points-2*num_buffer_points+i]
+function ApplyPeriodicBoundaryCondition!(
+    grid::Grid{NumState,NumDiagnostic}
+) where {NumState,NumDiagnostic}
+    base_level = grid.levels[1]
+    num_total_points = base_level.num_total_points
+    num_buffer_points = base_level.num_buffer_points
+    for v in 1:NumState
+        u = base_level.state[v]
+        for i in 1:num_buffer_points
+            u[i] = u[num_total_points - 2 * num_buffer_points + i]
         end
-        for i = num_total_points:-1:num_total_points-num_buffer_points+1
-            u[i] = u[2*num_buffer_points-num_total_points+i]
+        for i in num_total_points:-1:(num_total_points - num_buffer_points + 1)
+            u[i] = u[2 * num_buffer_points - num_total_points + i]
         end
     end
 end
@@ -18,13 +20,13 @@ end
 function ApplyPeriodicBoundaryConditionRHS!(level, r)
     num_total_points = level.num_total_points
     num_buffer_points = level.num_buffer_points
-    for v = 1:length(r)
+    for v in 1:length(r)
         rhs = r[v]
-        for i = 1:num_buffer_points
-            rhs[i] = rhs[num_total_points-2*num_buffer_points+i]
+        for i in 1:num_buffer_points
+            rhs[i] = rhs[num_total_points - 2 * num_buffer_points + i]
         end
-        for i = num_total_points:-1:num_total_points-num_buffer_points+1
-            rhs[i] = rhs[2*num_buffer_points-num_total_points+i]
+        for i in num_total_points:-1:(num_total_points - num_buffer_points + 1)
+            rhs[i] = rhs[2 * num_buffer_points - num_total_points + i]
         end
     end
 end
