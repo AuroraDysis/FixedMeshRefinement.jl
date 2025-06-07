786b0c8171566639f30d023999f4f7acdee1c91f
Zhen Zhong <auroradysis@gmail.com>
2025-06-07 03:42:46 +0100

    dev

---
+++ src/restrict.jl
@@ -0,0 +1,35 @@
+#===============================================================================
+restriction:
+    * from level l+1 to level l
+    * we assume that we always march fine level first (for l in lmax-1:-1:1)
+    * we assume all the levels are at the same time slice
+===============================================================================#
+function restriction!(grid, l; apply_trans_zone=false)
+    fine_level = grid.levels[l + 1]
+    coarse_level = grid.levels[l]
+
+    (; num_total_points, num_buffer_points, num_transition_points, parent_indices) =
+        fine_level
+
+    isrt = if apply_trans_zone
+        1 + num_buffer_points + num_transition_points
+    else
+        1 + num_buffer_points
+    end
+
+    iend = if apply_trans_zone
+        num_total_points - num_buffer_points - num_transition_points
+    else
+        num_total_points - num_buffer_points
+    end
+
+    uf = @view(fine_level.u[isrt:2:iend, :])
+    uc = coarse_level.u
+
+    if apply_trans_zone
+        noffset = div(num_transition_points + 1, 2)
+        uc[parent_indices[(1 + noffset):(end - noffset)], :] .= uf
+    else
+        uc[parent_indices, :] .= uf
+    end
+end
