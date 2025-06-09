47a9e3432341f20360dc3107d026c5538956a338
Zhen Zhong <auroradysis@gmail.com>
2025-06-09 03:46:31 +0100

    Update boundary.jl

--- examples/boundary.jl
+++ examples/boundary.jl
@@ -7,11 +7,11 @@ function apply_periodic_boundary_condition!(
     u = base_level.state[end]
 
     for i in 1:num_buffer_points
-        u[i] = @view(u[num_total_points - 2 * num_buffer_points + i])
+        u[i, :] = @view(u[num_total_points - 2 * num_buffer_points + i, :])
     end
 
     for i in num_total_points:-1:(num_total_points - num_buffer_points + 1)
-        u[i] = @view(u[2 * num_buffer_points - num_total_points + i])
+        u[i, :] = @view(u[2 * num_buffer_points - num_total_points + i, :])
     end
 end
 
