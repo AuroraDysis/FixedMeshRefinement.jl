31f8159596f1c7541fb5403c9769e718f3fab25f
Zhen Zhong <auroradysis@gmail.com>
2025-06-09 16:43:00 +0100

    dev

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,14 +1,13 @@
-using FixedMeshRefinement
-using Test
-using Aqua
-using JET
+@testitem "Code quality (Aqua.jl)" begin
+    using Aqua
 
-@testset "FixedMeshRefinement.jl" begin
-    @testset "Code quality (Aqua.jl)" begin
-        Aqua.test_all(FixedMeshRefinement)
-    end
-    @testset "Code linting (JET.jl)" begin
-        JET.test_package(FixedMeshRefinement; target_defined_modules = true)
-    end
-    # Write your tests here.
+    Aqua.test_all(FixedMeshRefinement)
 end
+
+@testitem "Code linting (JET.jl)" begin
+    using JET
+
+    JET.test_package(FixedMeshRefinement; target_defined_modules = true)
+end
+
+@run_package_tests
