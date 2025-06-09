7682b584adbe4188741b85659d390b6c20846e20
Zhen Zhong <auroradysis@gmail.com>
2025-06-09 03:31:45 +0100

    Update boundary.jl

--- examples/boundary.jl
+++ examples/boundary.jl
@@ -3,14 +3,15 @@ function apply_periodic_boundary_condition!(
 ) where {NumState,NumDiagnostic}
     base_level = grid.levels[1]
     (; num_total_points, num_buffer_points) = base_level
-    for v in 1:NumState
-        u = base_level.u[v]
-        for i in 1:num_buffer_points
-            u[i] = u[num_total_points - 2 * num_buffer_points + i]
-        end
-        for i in num_total_points:-1:(num_total_points - num_buffer_points + 1)
-            u[i] = u[2 * num_buffer_points - num_total_points + i]
-        end
+
+    u = base_level.state[end]
+
+    for i in 1:num_buffer_points
+        u[i] = @view(u[num_total_points - 2 * num_buffer_points + i])
+    end
+
+    for i in num_total_points:-1:(num_total_points - num_buffer_points + 1)
+        u[i] = @view(u[2 * num_buffer_points - num_total_points + i])
     end
 end
 
@@ -20,6 +21,7 @@ function apply_periodic_boundary_condition_rhs!(level, rhs)
     for i in 1:num_buffer_points
         rhs[i, :] .= @view(rhs[num_total_points - 2 * num_buffer_points + i, :])
     end
+
     for i in num_total_points:-1:(num_total_points - num_buffer_points + 1)
         rhs[i, :] .= @view(rhs[2 * num_buffer_points - num_total_points + i, :])
     end
