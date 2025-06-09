59070fa60b1c8f6f103fed5366cd760745a02093
Zhen Zhong <auroradysis@gmail.com>
2025-06-09 02:52:18 +0100

    Update intial_data.jl

--- examples/intial_data.jl
+++ examples/intial_data.jl
@@ -6,11 +6,12 @@ function gaussian!(grid; amp=1.0, sig=0.25, x0=0.0)
     (; levels, num_levels) = grid
     for l in 1:num_levels
         level = levels[l]
-        psi = level.u[1]
-        Pi = level.u[2]
+        u = level.state[end]
+        psi = u[1]
+        psi_t = u[2]
         x = level.x
         @. psi = amp * exp(-((x - x0) / sig)^2)
-        @. Pi = 0.0
+        @. psi_t = 0.0
     end
 
     # restriction for consistence
@@ -24,11 +25,12 @@ function sinusoidal!(grid)
 
     for l in 1:num_levels
         level = levels[l]
-        psi = level.u[1]
-        Pi = level.u[2]
+        u = level.state[end]
+        psi = u[1]
+        psi_t = u[2]
         x = level.x
         @. psi = sin(2 * pi * (x - 0.0))
-        @. Pi = -2 * pi * cos(2 * pi * (x - 0.0))
+        @. psi_t = -2 * pi * cos(2 * pi * (x - 0.0))
     end
 
     # restriction for consistence
@@ -52,16 +54,19 @@ function march_backwards!(grid)
         if l > 1
             prolongation!(grid, l, false)
         end
-        rk4!(levels[l], wave_rhs_backward!)
+        level = levels[l]
+        rk4!(level, wave_rhs_backward!)
+        state = level.state
         # save new u(-dt) -> u_p, u(0) -> u
-        u = levels[l].u
-        u_p = levels[l].u_p
-        u_pp = levels[l].u_pp
+        u = state[end]
+        u_p = state[end - 1]
+        u_pp = state[end - 2]
         @. u_pp = u_p
         @. u_p = u
         @. u = u_pp
-        levels[l].t = 0.0
+        level.t = 0.0
     end
     levels[1].t = 0.0
+    grid.t = 0.0
     return nothing
 end
