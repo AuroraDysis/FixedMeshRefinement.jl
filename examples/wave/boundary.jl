5042548d1e8311143f24c4089c75af6a0a055b8f
Zhen Zhong <auroradysis@gmail.com>
2025-06-10 03:09:28 +0100

    Update boundary.jl

--- examples/wave/boundary.jl
+++ examples/wave/boundary.jl
@@ -6,20 +6,24 @@ function apply_reflective_boundary_condition!(
     for l in 1:num_levels
         level = levels[l]
         u = level.state[end]
-        (; num_total_points, num_additional_points, additional_points_indices, is_physical_boundary) = level
+        (;
+            num_interior_points,
+            num_additional_points,
+            additional_points_indices,
+            is_physical_boundary,
+        ) = level
 
         # apply reflective boundary condition to state
         if is_physical_boundary[1]
             for i in 1:num_additional_points[1]
-                @. u[additional_points_indices[1][i], :] = -@view(u[num_additional_points[1] + i, :])
+                @. u[additional_points_indices[1][i], :] = -@view(u[i, :])
             end
         end
 
         if is_physical_boundary[2]
             for i in 1:num_additional_points[2]
-                @. u[additional_points_indices[2][i], :] = -@view(
-                    u[num_total_points - num_additional_points[2] + 1 - i, :]
-                )
+                @. u[additional_points_indices[2][i], :] =
+                    -@view(u[num_interior_points + 1 - i, :])
             end
         end
     end
@@ -30,20 +34,24 @@ end
 function apply_reflective_boundary_condition_rhs!(
     level::Level{NumState,NumDiagnostic}, rhs
 ) where {NumState,NumDiagnostic}
-    (; num_total_points, num_additional_points, additional_points_indices, is_physical_boundary) = level
+    (;
+        num_interior_points,
+        num_additional_points,
+        additional_points_indices,
+        is_physical_boundary,
+    ) = level
 
     # apply reflective boundary condition to state
     if is_physical_boundary[1]
         for i in 1:num_additional_points[1]
-            @. rhs[additional_points_indices[1][i], :] = -@view(rhs[num_additional_points[1] + i, :])
+            @. rhs[additional_points_indices[1][i], :] = -@view(rhs[i, :])
         end
     end
 
     if is_physical_boundary[2]
         for i in 1:num_additional_points[2]
-            @. rhs[additional_points_indices[2][i], :] = -@view(
-                rhs[num_total_points - num_additional_points[2] + 1 - i, :]
-            )
+            @. rhs[additional_points_indices[2][i], :] =
+                -@view(rhs[num_interior_points + 1 - i, :])
         end
     end
 
