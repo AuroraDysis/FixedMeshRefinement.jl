c062522e76ddd8f36927fda7d498d37c1aed3d68
Zhen Zhong <auroradysis@gmail.com>
2025-06-11 11:48:27 +0100

    Update output.jl

--- examples/wave/output.jl
+++ examples/wave/output.jl
@@ -8,8 +8,8 @@ function write_output(dir_path, grid, it)
         h5open(joinpath(dir_path, "data.it$(lpad(it, 6, '0')).h5"), "w") do file
             for l in 1:num_levels
                 level = levels[l]
-                u = get_state(level)
                 x = get_x(level)
+                u = get_state(level)
                 num_interior_indices = get_interior_indices(level)
 
                 g = create_group(file, "level$(lpad(l, 2, '0'))")
