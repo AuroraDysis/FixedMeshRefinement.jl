3b9becfb511d97f2d3c4de94add0ef10c76bc37a
Zhen Zhong <auroradysis@gmail.com>
2025-07-08 14:53:34 +0100

    dev

--- src/utils.jl
+++ src/utils.jl
@@ -1,4 +1,4 @@
-export typetol
+export typetol, extrapolate!
 
 """
     typetol(TF::Type{<:AbstractFloat})
@@ -29,4 +29,26 @@ end
 function isapprox_tol(a::T, b::T) where {T<:AbstractFloat}
     tol = typetol(T)
     return isapprox(a, b; atol=tol, rtol=tol)
+end
+
+function extrapolate!(out, input, order::Integer)
+    length(input) == order || error("Length of input must be equal to order.")
+
+    if order == 1
+        @.. out = input[1]
+    elseif order == 2
+        @.. out = 2 * input[1] - input[2]
+    elseif order == 3
+        @.. out = 3 * input[1] - 3 * input[2] + input[3]
+    elseif order == 4
+        @.. out = 4 * input[1] - 6 * input[2] + 4 * input[3] - input[4]
+    elseif order == 5
+        @.. out = 5 * input[1] - 10 * input[2] + 10 * input[3] - 5 * input[4] + input[5]
+    elseif order == 6
+        @.. out =
+            6 * input[1] - 15 * input[2] + 20 * input[3] - 15 * input[4] + 6 * input[5] -
+            input[6]
+    else
+        error("Extrapolation order $order is not supported.")
+    end
 end
